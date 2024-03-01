2dcceda8bf6dcb4f27c70afc3b737eaf4ed3a8ca
Alessandra Lucas <alessandra1702@hotmail.com>
2024-03-01 20:08:36 -0300

    instancia de classe
    instanciando a classe Curso e visualizando a disponibilidade das propriedades existentes para uso

--- Balta/Program.cs
+++ Balta/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.InteropServices;
+using Balta.ContextoConteudo;
+
+namespace Balta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var curso = new Curso();
+            curso.
+
+        }
+    }
+}
+
