4e82c51e461dc5427aa1a3813cee549a657a1a9a
Alessandra Lucas <alessandra1702@hotmail.com>
2024-03-04 21:15:30 -0300

    Add class Enumeradores

---
+++ Balta/ContextoConteudo/Enumeradores/ENivelConteudo.cs
@@ -0,0 +1,11 @@
+namespace Balta.ContextoConteudo.Enumeradores
+{
+    // nivel do meu conteudo
+    public enum ENivelConteudo
+    {
+        Iniciante = 1,
+        Fundamental = 2,
+        Intermediario = 3,
+        Avancado = 4,
+    }
+}
