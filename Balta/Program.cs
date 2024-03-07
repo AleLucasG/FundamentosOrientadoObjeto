1988fd08a00a422aed2ea63f07239911403ae2e4
Alessandra Lucas <alessandra1702@hotmail.com>
2024-03-07 19:16:57 -0300

    Modelando a carreira

--- Balta/Program.cs
+++ Balta/Program.cs
@@ -13,8 +13,16 @@ namespace Balta
             var curso = new Curso();
 
             curso.Nivel = ContextoConteudo.Enumeradores.ENivelConteudo.Fundamental;
-            foreach (var item in curso.Modulos){}
             
+            foreach (var item in curso.Modulos)
+            {
+
+            }
+            
+            // faz a contagem da classe Carreira
+            var carreira = new Carreira();
+            carreira.Items.Add(new ItemCarreira());
+            Console.WriteLine(carreira.TotalCursos);
 
         }
     }
