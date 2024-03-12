67da813abf72455bf6bc1b8ef1a2b206afaa761d
Alessandra Lucas <alessandra1702@hotmail.com>
2024-03-11 22:56:53 -0300

    Interagindo com listas

--- Balta/Program.cs
+++ Balta/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using Balta.ContextoConteudo;
@@ -14,11 +15,41 @@ namespace Balta
             artigos.Add(new Artigo("Artigo sobre C#", "csharp"));
             artigos.Add(new Artigo("Artigo sobre .Net", "dotnet"));
 
-            foreach(var artigo in artigos)
+            //foreach(var artigo in artigos)
+            //{
+            //    Console.WriteLine(artigo.Id);
+            //    Console.WriteLine(artigo.Titulo);
+            //    Console.WriteLine(artigo.Url);
+            //}
+
+            var curso = new List<Curso>();
+            var cursoPOO = new Curso("Fundamentos POO", "fundamentos-poo");
+            var cursoCSharp = new Curso("Fundamentos POO", "fundamentos-csharp");
+            var cursoAspNet = new Curso("Fundamentos POO", "fundamentos-aspnet");
+
+            curso.Add(cursoPOO);
+            curso.Add(cursoCSharp);
+            curso.Add(cursoAspNet);
+
+            var carreiras = new List<Carreira>();
+            var carreiraDotNet = new Carreira("Especialista .Net", "especialista-dotnet");
+            var itemCarreira2 = new ItemCarreira(2, "Aprenda POO", "", null);
+            var itemCarreira = new ItemCarreira(1, "Comece por aqui", "", null);
+            var itemCarreira3 = new ItemCarreira(3, "Aprenda .Net", "", null);
+
+            carreiraDotNet.Items.Add(itemCarreira2);
+            carreiraDotNet.Items.Add(itemCarreira3);
+            carreiraDotNet.Items.Add(itemCarreira);
+
+            carreiras.Add(carreiraDotNet);
+
+            foreach(var carreira in carreiras)
             {
-                Console.WriteLine(artigo.Id);
-                Console.WriteLine(artigo.Titulo);
-                Console.WriteLine(artigo.Url);
+                Console.WriteLine(carreira.Titulo);
+                foreach(var item in carreira.Items.OrderBy(x=>x.Ordem))
+                {
+                    Console.WriteLine($"{item.Ordem} - {item.Titulo}");
+                }
             }
 
         }
