c171fb185285a0ea6033cdc9d964531360427c52
Alessandra Lucas <alessandra1702@hotmail.com>
2024-03-18 20:47:47 -0300

    Classe Plano
    O que se refere a um plano de assinatura, como titulo do plano, valor do plano

---
+++ Balta/ContextoAssinatura/Plano.cs
@@ -0,0 +1,11 @@
+using Balta.ContextoCompartilhado;
+
+namespace Balta.ContextoAssinatura
+{
+    public class Plano : Base
+    {
+        public string Titulo { get; set; }
+
+        public decimal Preco { get; set; }
+    }
+}
